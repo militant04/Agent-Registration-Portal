8014433146d2287950de9c02be2e4afb0cefe12e
Militant Saungweme <militant04@users.noreply.github.com>
2019-01-23 22:10:44 +0200

    commit dir

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/auth"
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loggedIn : boolean;
+  loginMessage =null;
+  signin = 'Sign In';
+
+  user = {"username": "","password": ""};
+  constructor(public afAuth: AngularFireAuth, private router: Router){
+    this.loggedIn = false;
+  }
+  title = 'app';
+  ngOnInit() {
+
+  }
+
+  async signup(){
+
+    try{
+
+      this.signin = '.....Signing In';
+
+      console.log('username'+this.user.username);
+      const result = this.afAuth.auth.signInWithEmailAndPassword(this.user.username,this.user.password)
+        .then(auth => {
+          this.loggedIn =true;
+          this.router.navigateByUrl('admin');
+        })
+        .catch(err => {
+          console.log(err.code);
+
+          this.loginMessage =err.message;
+          this.signin = 'Try Sign In Again'
+
+        });
+      console.log(result);
+
+
+    }
+    catch (error){
+      console.log(error);
+      // loading.dismiss();
+    }
+
+
+
+  }
+
+}
